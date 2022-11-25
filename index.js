e37cb5f1e9e5083f6267d19401e9b0272b1d19fe
Freddie16 <freddiemurigi@gmail.com>
2022-11-25 16:08:18 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (x,y) => {
+    console.log(`Dividing ${x} by ${y}`);
+    return 20
+}
+divide(2000,100)
+
+
+const square = number => number ** number;
+
+
+const add = (parameter1,parameter2) => {
+    console.log(`Add ${parameter1} to ${parameter2}`)
+    return 7
+}
+add(3,4)
+
